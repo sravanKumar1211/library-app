21492757f1bd5d3192a69fc7e4ac602112dc4ef6
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-11-08 17:41:13 +0530

    comments and description of slice was added

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,18 @@
-
+// Importing 'configureStore' from Redux Toolkit
+// This function helps create a Redux store with good defaults (like Redux DevTools)
 import { configureStore } from "@reduxjs/toolkit";
+
+// Importing the reducer for the 'book' slice
 import bookReducer from "./bookSlice";
 
+// Creating the Redux store
 const store = configureStore({
   reducer: {
+    // Registering the 'book' slice reducer
+    // 'book' will be the key in the Redux state (state.book)
     book: bookReducer
   }
 });
 
+// Exporting the store so it can be provided to the React app
 export default store;
